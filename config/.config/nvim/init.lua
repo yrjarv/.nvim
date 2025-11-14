1d96c43385bb64f45a42b2553ca64ea4b3fa9eb2
Yrjar Vederhus <yrjar@yrjar.tech>
2025-11-14 15:05:05 +0100

    feat: add gitgutter and noh keybind

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -8,11 +8,13 @@ Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('jiangmiao/auto-pairs')
 Plug('NStefan002/screenkey.nvim')
+Plug('airblade/vim-gitgutter')
 vim.call('plug#end')
 
 home = os.getenv("HOME")
 package.path = home .. "/.config/nvim/?.lua;" .. package.path
 require"common"
+require"keybinds"
 require"theme"
 require"treesitter"
 require"lsp"
